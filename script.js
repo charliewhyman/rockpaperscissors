6d037f0124c4c091d4ed500072d27ba7f9e1d232
cwhyman <cwhyma@gmail.com>
2021-06-15 22:14:27 +0100

    Added basic logic functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-resultArray = ['Rock', 'Paper', 'Scissors']
+resultArray = ['ROCK', 'PAPER', 'SCISSORS']
 
 function computerPlay() {
     /* generate a random number between 1 and resultArray length */
@@ -7,3 +7,23 @@ function computerPlay() {
     /* return the randomly selected value from the result array */
     return resultArray[randomNumber];
 };
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === "ROCK" && computerSelection === !"ROCK") {
+
+    }
+    
+    else if (playerSelection === "ROCK" && computerSelection === !"ROCK") {
+
+    }
+    
+    else return "Not valid"
+    
+    
+}
+
+const playerSelection = 'Rock'.toUpperCase();
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
