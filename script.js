2e2f7204c0457e73c5441e2f8073ea53eca8148f
cwhyman <cwhyma@gmail.com>
2021-06-16 17:10:45 +0100

    Fixed logic and result messages

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
-resultArray = ['ROCK', 'PAPER', 'SCISSORS']
+let resultArray = ["ROCK", "PAPER", "SCISSORS"];
+let playerScore = 0;
 
 function computerPlay() {
-    /* generate a random number between 1 and resultArray length */
+    /* Generate a random number between 1 and resultArray length */
     randomNumber = Math.floor(Math.random() * resultArray.length);
 
-    /* return the randomly selected value from the result array */
+    /* Return the randomly selected value from the result array */
     return resultArray[randomNumber];
 };
 
+    /* Define a function to play a single round of rock, paper scissors */
 function playRound(playerSelection, computerSelection) {
-    if (!resultArray.includes(playerSelection)) {
-        'Not a valid input'
+
+    /* Create if statements to compare player and computer selections */
+    if (playerSelection === computerSelection) {
+        return "Draw!"
     }
     
-    else if (playerSelection === 'PAPER') {
-        return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+    else if (playerSelection === "PAPER") {
+        return loseMessage;
     }
 
-    else if (playerSelection === 'ROCK' && (computerSelection === 'SCISSORS' || computerSelection === 'PAPER')) {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    else if (playerSelection === "ROCK" && (computerSelection === "SCISSORS" || computerSelection === "PAPER")) {
+        playerScore++;
+        return winMessage;
     }
    
-    else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        playerScore++;
+        return winMessage;
     }
 
-    else return 'Draw!';
+    else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        playerScore++;
+        return loseMessage;
+    }
+
+    else return "Not a valid input";
 }
 
-const playerInput = 'Rock';
+/* Define a function to play a 5 round game that keeps score, and reports a winner/loser */
+function game() {
+    for (let step = 0; step < 5; step++) {
+        // Runs 5 times, with values of step 0 through 4.
+        playRound(playerSelection, computerSelection);
+      }
+}
+
+const playerInput = 'rock'
+/* window.prompt("Enter your choice (rock, paper or scissors)"); */
 const playerSelection = playerInput.toUpperCase();
 const computerSelection = computerPlay();
-console.log(playerInput);
 
-console.log(playRound(playerSelection, computerSelection));
+const loseMessage = `You lose! Computer's ${computerSelection} beats your ${playerSelection}`;
+const winMessage = `You win! Computer's ${computerSelection} beats your ${computerSelection}`;
+
+console.log("player" + playerSelection);
+console.log("computer" + computerSelection);
+console.log(game())
