947440c5187173df6c4c229bff70f40d659f141e
cwhyman <cwhyma@gmail.com>
2021-07-07 22:48:21 +0100

    Changed to single game, fixed loop logic

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let resultArray = ["ROCK", "PAPER", "SCISSORS"];
 let playerScore = 0;
 let computerScore = 0;
 
-let playerInput = window.prompt("Enter your choice (rock, paper or scissors)");
-const playerSelection = playerInput.toUpperCase();
 let computerSelection = computerPlay();
 
-const loseMessage = `You lose! Computer's ${computerSelection} beats your ${playerSelection}`;
-const winMessage = `You win! Your ${playerSelection} beats Computer's ${computerSelection}`;
+function playerPlay() {
+    let playerInput = window.prompt("Enter your choice (rock, paper or scissors)");
+    const playerSelection = playerInput.toUpperCase();
+    return playerSelection;
+}
 
 function computerPlay() {
     /* Generate a random number between 1 and resultArray length */
@@ -19,6 +20,8 @@ function computerPlay() {
 
     /* Define a function to play a single round of rock, paper scissors */
 function playRound(playerSelection, computerSelection) {
+    const loseMessage = `You lose! Computer's ${computerSelection} beats your ${playerSelection}`;
+    const winMessage = `You win! Your ${playerSelection} beats Computer's ${computerSelection}`;
 
     /* Create if statements to compare player and computer selections */
     if (playerSelection === computerSelection) {
@@ -50,16 +53,13 @@ function playRound(playerSelection, computerSelection) {
 
 /* Define a function to play a 5 round game that keeps score, and reports a winner/loser */
 function game() {
-    for (let step = 0; step < 5; step++) {
-        let playerInput = window.prompt("Enter your choice (rock, paper or scissors)");
-        /* window.prompt("Enter your choice (rock, paper or scissors)"); */
-        let computerSelection = computerPlay();
+        
         /* Runs 5 times, with values of step 0 through 4. */
-        console.log(playRound(playerSelection, computerSelection));        
-      }
+        console.log(playRound(playerPlay(), computerPlay()));        
+      
 
     if (playerScore === computerScore) {
-        return `Draw! Both players scored !{playerScore}`;
+        return `Draw! Both players scored ${playerScore}`;
     }
 
     else if (playerScore > computerScore) {
